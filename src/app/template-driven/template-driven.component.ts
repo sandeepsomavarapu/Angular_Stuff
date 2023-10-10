068517cfab9c381a13d8a738eb96ee91b9751c99
sandeep <sandeepsomavarapu535@gmail.com>
2023-10-10 12:31:18 +0530

    Template driven with validations

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -8,26 +8,17 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateDrivenComponent {
   constructor() { }
+  onSubmit(data:NgForm)
+  {
+    console.log(data)
+    console.log(data.value)
+    alert(data.value.id)
+    alert(data.value.name)
+    alert(data.value.salary)
+    alert(data.value.phonenumber)
+    alert(data.value.company)
 
-  onSubmit(contactForm1:NgForm) {
-    console.log(contactForm1)
-
-    alert(contactForm1.value.email);
-    alert(contactForm1.value.lastname);
-    alert(contactForm1.value.firstname);
-    alert(contactForm1.value.country);
-    alert(contactForm1.value.gender);
   }
-  countryList:country[] = [
-    new country("1", "India"),
-    new country('2', 'USA'),
-    new country('3', 'England')
-  ];
+  
+
 }
-export class country {
-  id:string;
-  name:string;
-  constructor(id:string, name:string) {
-    this.id=id;
-    this.name=name;
-  }}
